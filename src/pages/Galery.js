ba7a793fb46c1f8e004aabeb7a9423e82301b725
Łukasz <lukasz.poczta12@gmail.com>
2023-03-15 19:05:33 +0100

    started gallery page

--- src/pages/Galery.js
+++ src/pages/Galery.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from '../style/Galery.module.css';
+
+const importAll = (r) => r.keys().map(r);
+const images = importAll(require.context('../../images', false));
 
 const Galery = () => {
-	return <div>Galery</div>;
+	// const ImageQty = Math.floor(images.length / 2);
+	const [activeIndex, setActiveIndex] = useState(1);
+
+	return (
+		<div className={classes.mainContainer}>
+			<div className={classes.bigImage}>
+				<img src={images[activeIndex]} alt={'zdjęcie główne'} className={classes.mainPhoto} />
+			</div>
+			<div className={classes.imagesContainer}>
+				{images.map((image, index) => (
+					<div className={classes.imageBox}>
+						{activeIndex === index ? (
+							<img
+								key={index}
+								src={image}
+								alt={`${index}`}
+								className={`${classes.image} ${classes.active}`}
+							/>
+						) : (
+							<img
+								key={index}
+								src={image}
+								alt={`${index}`}
+								className={`${classes.image}`}
+							/>
+						)}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Galery;
