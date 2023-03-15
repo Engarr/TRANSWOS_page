dec5a5261831e74f4675b08dfcf994b55fd1862b
Łukasz <lukasz.poczta12@gmail.com>
2023-03-15 19:31:52 +0100

    changed gallery

--- src/pages/Galery.js
+++ src/pages/Galery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from '../style/Galery.module.css';
 
 const importAll = (r) => r.keys().map(r);
@@ -6,12 +6,24 @@ const images = importAll(require.context('../../images', false));
 
 const Galery = () => {
 	// const ImageQty = Math.floor(images.length / 2);
-	const [activeIndex, setActiveIndex] = useState(1);
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const indexhandlerOnClick = (e) => {
+		setActiveIndex(e.target.alt);
+		console.log(activeIndex);
+	};
+	useEffect(() => {
+		
+	}, []);
 
 	return (
 		<div className={classes.mainContainer}>
 			<div className={classes.bigImage}>
-				<img src={images[activeIndex]} alt={'zdjęcie główne'} className={classes.mainPhoto} />
+				<img
+					src={images[activeIndex]}
+					alt={'zdjęcie główne'}
+					className={classes.mainPhoto}
+				/>
 			</div>
 			<div className={classes.imagesContainer}>
 				{images.map((image, index) => (
@@ -22,6 +34,7 @@ const Galery = () => {
 								src={image}
 								alt={`${index}`}
 								className={`${classes.image} ${classes.active}`}
+								onClick={indexhandlerOnClick}
 							/>
 						) : (
 							<img
@@ -29,6 +42,7 @@ const Galery = () => {
 								src={image}
 								alt={`${index}`}
 								className={`${classes.image}`}
+								onClick={indexhandlerOnClick}
 							/>
 						)}
 					</div>
