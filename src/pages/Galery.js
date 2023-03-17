8f4a4d04029faf262fa0a519bc45c7dc1ae891da
Łukasz <lukasz.poczta12@gmail.com>
2023-03-17 09:59:41 +0100

    changed logic on gallery component

--- src/pages/Galery.js
+++ src/pages/Galery.js
@@ -1,5 +1,7 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
+import { MdOutlineClose } from 'react-icons/md';
+import { GrNext, GrPrevious } from 'react-icons/gr';
 import classes from '../style/Galery.module.css';
 
 const importAll = (r) => r.keys().map(r);
@@ -8,6 +10,7 @@ const images = importAll(require.context('../../images', false));
 const Galery = () => {
 	// const ImageQty = Math.floor(images.length / 2);
 	const [activeIndex, setActiveIndex] = useState(0);
+	const [zoomed, setZoomed] = useState(false);
 
 	const imgLength = images.length - 1;
 
@@ -17,6 +20,7 @@ const Galery = () => {
 	};
 
 	const indexHandlerRight = (e) => {
+		e.stopPropagation();
 		if (activeIndex >= 0) {
 			setActiveIndex((prev) => prev + 1);
 		}
@@ -24,7 +28,8 @@ const Galery = () => {
 			setActiveIndex(0);
 		}
 	};
-	const indexHandlerLeft = () => {
+	const indexHandlerLeft = (e) => {
+		e.stopPropagation();
 		if (activeIndex > 0) {
 			setActiveIndex((prev) => prev - 1);
 		}
@@ -32,50 +37,82 @@ const Galery = () => {
 			setActiveIndex(imgLength);
 		}
 	};
+	const zoomHandler = (e) => {
+		e.stopPropagation();
+		setZoomed((prev) => !prev);
+	};
+
 
 	return (
-		<div className={classes.mainContainer}>
-			<div className={classes.bigImage}>
-				<div className={classes.mainPhotoBox}>
-					<div className={classes.switchBtnLeft} onClick={indexHandlerLeft}>
-						<GoArrowLeft className={classes.icon} />
-					</div>
-					<img
-						src={images[activeIndex]}
-						alt={'zdjęcie główne'}
-						className={classes.mainPhoto}
-					/>
-					<div className={classes.switchBtnRight} onClick={indexHandlerRight}>
-						<GoArrowRight className={classes.icon} />
+		<>
+			{zoomed && (
+				<div className={classes.zoomedBg} onClick={zoomHandler}>
+					<div>
+						<div className={classes.closeBox}>
+							<MdOutlineClose
+								className={classes.closeIcon}
+								onClick={zoomHandler}
+							/>
+						</div>
+						<div onClick={indexHandlerLeft} className={classes.prevBox}>
+							<GrPrevious className={classes.zoomedIconPrev} />
+						</div>
+						<img
+							src={images[activeIndex]}
+							alt={'zdjęcie główne'}
+							className={`${classes.zoomedImg} `}
+							onClick={(e) => e.stopPropagation()}
+						/>
+						<div onClick={indexHandlerRight} className={classes.nextBox}>
+							<GrNext className={classes.zoomedIconNext} />
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className={classes.imagesContainer}>
-				{images.map((image, index) => (
-					<div className={classes.imageBox}>
-						{activeIndex === index ? (
-							<img
-								key={index}
-								src={image}
-								alt={`${index}`}
-								data-index={index}
-								className={`${classes.image} ${classes.active}`}
-								onClick={indexhandlerOnClick}
-							/>
-						) : (
-							<img
-								key={index}
-								src={image}
-								data-index={index}
-								alt={`${index}`}
-								className={`${classes.image}`}
-								onClick={indexhandlerOnClick}
-							/>
-						)}
+			)}
+			<div className={classes.mainContainer}>
+				<div className={classes.bigImage}>
+					<div className={classes.mainPhotoBox}>
+						<div className={classes.switchBtnLeft} onClick={indexHandlerLeft}>
+							<GoArrowLeft className={classes.icon} />
+						</div>
+						<img
+							src={images[activeIndex]}
+							alt={'zdjęcie główne'}
+							className={`${classes.mainPhoto} `}
+							onClick={zoomHandler}
+						/>
+						<div className={classes.switchBtnRight} onClick={indexHandlerRight}>
+							<GoArrowRight className={classes.icon} />
+						</div>
 					</div>
-				))}
+				</div>
+				<div className={classes.imagesContainer}>
+					{images.map((image, index) => (
+						<div className={classes.imageBox}>
+							{activeIndex === index ? (
+								<img
+									key={index}
+									src={image}
+									alt={`${index}`}
+									data-index={index}
+									className={`${classes.image} ${classes.active}`}
+									onClick={indexhandlerOnClick}
+								/>
+							) : (
+								<img
+									key={index}
+									src={image}
+									data-index={index}
+									alt={`${index}`}
+									className={`${classes.image}`}
+									onClick={indexhandlerOnClick}
+								/>
+							)}
+						</div>
+					))}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
